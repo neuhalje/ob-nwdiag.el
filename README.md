5023d7bbdaed13bff22e4d3adfa5dfa66cce6648
Jens Neuhalfen <jens@neuhalfen.name>
2021-05-04 08:37:37 +0200

    Clone to nwdiag

--- README.md
+++ README.md
@@ -1,46 +1,40 @@
-ob-blockdiag.el
+ob-nwdiag.el
 ---------------
 
-An extension for org-babel to support [blockdiag](http://blockdiag.com/en/).
+An extension for org-babel to support [nwdiag](http://blockdiag.com/en/).
 
 ## Installation
 
-This package is availaple in [MELPA](https://melpa.org/).
-
-> [How to enable MELPA](https://melpa.org/#/getting-started).
-
-With "MELPA enabled emacs" you could install this package by typing this sequence:
-
-``` text
-M-x
-package-install
-ob-blockdiag
-⏎
-```
+Please install from the repo.
 
 Now you could enable this package in your Emacs config with:
 
 ``` emacs-lisp
 (org-babel-do-load-languages 'org-babel-load-languages
- '((blockdiag . t))
+ '((nwdiag . t))
 )
 ```
 
 ## Example
 
-> Blockdiag in Fedora installed from `python3-blockdiag` package
-> which has unusual executable name.
-> Output file name is `ololo.png`
 
 ``` org
-#+BEGIN_SRC blockdiag :tool blockdiag-3.5 :file ololo.png
-blockdiag {
-   A [label = "foo"];
-   B [label = "bar"];
-   C [label = "baz"];
-   A -> B [label = "click bar", textcolor="red"];
-   B -> C [label = "click baz"];
-   C -> A;
+#+BEGIN_SRC nwdiag :tool nwdiag-3.5 :file ololo.png
+nwdiag {
+  network dmz {
+      address = "210.x.x.x/24"
+
+      web01 [address = "210.x.x.1"];
+      web02 [address = "210.x.x.2"];
+  }
+  network internal {
+      address = "172.x.x.x/24";
+
+      web01 [address = "172.x.x.1"];
+      web02 [address = "172.x.x.2"];
+      db01;
+      db02;
+  }
 }
 #+END_SRC
 ```
@@ -49,7 +43,7 @@ blockdiag {
 
 There are some things you could customize:
 
-    - `:tool` if you have blockdiag in unusual location or want to use seqdiag or [others](http://blockdiag.com/en/#table-of-contents)
+    - `:tool` if you have nwdiag in unusual location or want to use seqdiag or [others](http://blockdiag.com/en/#table-of-contents)
     - `:transparency t` to make transparent background of diagram (PNG only)
     - `:antialias t` pass diagram image to anti-alias filter
     - `:font` if you want the `:tool` to use provided font
@@ -58,6 +52,6 @@ There are some things you could customize:
 
 ## Errors and output
 
-It recreates a buffer with name `*ob-blockdiag*` every time you evaluating something.
+It recreates a buffer with name `*ob-nwdiag*` every time you evaluating something.
 
 There will be errors and output.
--- ob-nwdiag.el
+++ ob-nwdiag.el
@@ -1,5 +1,8 @@
-;;; ob-blockdiag.el --- org-babel functions for blockdiag evaluation
+;;; ob-nwdiag.el --- org-babel functions for nwdiag evaluation
 
+;; This is a fork of ob-blockdiag
+
+;; Copyright (C) 2021 Jens Neuhalfen
 ;; Copyright (C) 2017 Dmitry Moskowski
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -15,39 +18,39 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Author: Dmitry Moskowski
+;; Author: Dmitry Moskowski / Jens Neuhalfen
 ;; Keywords: tools, convenience
 ;; Package-Version: 20170501.112
-;; Homepage: https://github.com/corpix/ob-blockdiag.el
+;; Homepage: https://github.com/neuhalje/ob-nwdiag.el
 
 ;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
 
-;; Org-Babel support for evaluating blockdiag source code.
+;; Org-Babel support for evaluating nwdiag source code.
 
 ;;; Requirements:
 
-;; - blockdiag :: http://blockdiag.com/en/
+;; - nwdiag :: http://nwdiag.com/en/
 
 ;;; Code:
 (require 'ob)
 
-(defvar org-babel-default-header-args:blockdiag
+(defvar org-babel-default-header-args:nwdiag
   '(
     (:results . "file")
     (:exports . "results")
-    (:tool    . "blockdiag")
+    (:tool    . "nwdiag")
     (:transparency . nil)
     (:antialias . nil)
     (:font    . nil)
     (:size    . nil)
     (:type    . nil))
-  "Default arguments for drawing a blockdiag image.")
+  "Default arguments for drawing a nwdiag image.")
 
-(add-to-list 'org-src-lang-modes '("blockdiag" . blockdiag))
+(add-to-list 'org-src-lang-modes '("nwdiag" . nwdiag))
 
-(defun org-babel-execute:blockdiag (body params)
+(defun org-babel-execute:nwdiag (body params)
   (let ((file (cdr (assoc :file params)))
         (tool (cdr (assoc :tool params)))
         (transparency (cdr (assoc :transparency params)))
@@ -56,11 +59,11 @@
         (size (cdr (assoc :size params)))
         (type (cdr (assoc :type params)))
 
-        (buffer-name "*ob-blockdiag*")
+        (buffer-name "*ob-nwdiag*")
         (error-template "Subprocess '%s' exited with code '%d', see output in '%s' buffer"))
     (save-window-excursion
       (let ((buffer (get-buffer buffer-name)))(if buffer (kill-buffer buffer-name) nil))
-      (let ((data-file (org-babel-temp-file "blockdiag-input"))
+      (let ((data-file (org-babel-temp-file "nwdiag-input"))
             (args (append (list "-o" file)
                           (if transparency (list) (list "--no-transparency"))
                           (if antialias (list "--antialias") (list))
@@ -73,5 +76,5 @@
             ((exit-code (apply 'call-process tool nil buffer nil (append args (list data-file)))))
           (if (= 0 exit-code) nil (error (format error-template tool exit-code buffer-name))))))))
 
-(provide 'ob-blockdiag)
-;;; ob-blockdiag.el ends here
+(provide 'ob-nwdiag)
+;;; ob-nwdiag.el ends here
